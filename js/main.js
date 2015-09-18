b07affa64869f38dece6ecf1867046271aa479f3
CharlesAMoss <charlesamoss@yahoo.com>
2015-09-18 09:53:16 -0700

    add 1st test pass

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function Pizza(pizzaSize, pizzaQuantity, pizzaTopping) {
+    this.pizzaSize = pizzaSize;
+    this.pizzaQuantity = pizzaQuantity;
+    this.pizzaTopping = [];
+}
--- spec/specs.js
+++ spec/specs.js
@@ -3,6 +3,6 @@ describe('Pizza', function() {
     var testPizza = new Pizza("Large Pie", 1);
     expect(testPizza.pizzaSize).to.equal("Large Pie");
     expect(testPizza.pizzaQuantity).to.equal(1);
-    expect(testPizza.pizzaToppings).to.eql([]);
+    expect(testPizza.pizzaTopping).to.eql([]);
   });
 });
