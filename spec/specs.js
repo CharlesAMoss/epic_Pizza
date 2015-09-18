7eac7247dffe9f0cda536833ddfc16805931f819
CharlesAMoss <charlesamoss@yahoo.com>
2015-09-18 09:44:47 -0700

    add first test and fail

--- spec/specs.js
+++ spec/specs.js
@@ -1,1 +1,8 @@
-
+describe('Pizza', function() {
+  it("creates a new pizza with the given specifications", function() {
+    var testPizza = new Pizza("Large Pie", 1);
+    expect(testPizza.pizzaSize).to.equal("Large Pie");
+    expect(testPizza.pizzaQuantity).to.equal(1);
+    expect(testPizza.pizzaToppings).to.eql([]);
+  });
+});
