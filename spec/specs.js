096c8f1946ec5a3d79fac21e75f2d9c55dabff1d
CharlesAMoss <charlesamoss@yahoo.com>
2015-09-18 10:14:15 -0700

    add 2nd test and fail

--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,18 @@
 describe('Pizza', function() {
-  it("creates a new pizza with the given specifications", function() {
-    var testPizza = new Pizza("Large Pie", 1);
-    expect(testPizza.pizzaSize).to.equal("Large Pie");
-    expect(testPizza.pizzaQuantity).to.equal(1);
-    expect(testPizza.pizzaTopping).to.eql([]);
-  });
+
+    it("creates a new pizza with the given specifications", function() {
+        var testPizza = new Pizza("Pie", 1);
+
+        expect(testPizza.pizzaSize).to.equal("Pie");
+        expect(testPizza.pizzaQuantity).to.equal(1);
+        expect(testPizza.pizzaTopping).to.eql([]);
+    });
+
+    it("calculates the cost for one small size no toppings", function() {
+        var testPizza = new Pizza("Slice", 1);
+        var price = 2.50;
+        var total = testPizza.total();
+
+        expect(total).to.equal(cost);
+    });
 });
